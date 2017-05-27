645b93e4641e7b9650ad603fed30eaa66494abaf
Zygimantus <zygimantus@gmail.com>
2017-05-27 17:12:15 +0300

    SeleniumTest added

---
+++ src/test/java/com/zygimantus/apiwebas/SeleniumTest.java
@@ -0,0 +1,29 @@
+package com.zygimantus.apiwebas;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ *
+ * @author Zygimantus
+ */
+public class SeleniumTest {
+
+    @Test
+    public void testSimple() throws Exception {
+
+        System.setProperty("webdriver.gecko.driver", "/bin/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("http://localhost:8080");
+
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals("Login Apiwebas", actualTitle);
+
+        driver.quit();
+    }
+
+}
