32d1910b7826924a1ffc47f48f78b7c8afc4cf86
Zygimantus <zygimantus@gmail.com>
2017-12-19 13:15:27 +0200

    Default User constructor

--- src/main/java/com/zygimantus/apiwebas/vaadin/model/User.java
+++ src/main/java/com/zygimantus/apiwebas/vaadin/model/User.java
@@ -7,45 +7,48 @@ import javax.persistence.Id;
 
 @Entity
 public class User {
-	
-	@Id
-	@GeneratedValue
-	@Column(name = "id")
-	private Long id;
-	
-	@Column(name = "user_name")
-	private String userName;
-	
-	@Column(name = "password")
-	private String password;
-	
-	public User(String userName, String password){
-		this.password = password;
-		this.userName = userName;
-	}
 
-	public Long getId() {
-		return id;
-	}
+        @Id
+        @GeneratedValue
+        @Column(name = "id")
+        private Long id;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+        @Column(name = "user_name")
+        private String userName;
 
-	public String getUserName() {
-		return userName;
-	}
+        @Column(name = "password")
+        private String password;
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+        public User() {
+        }
 
-	public String getPassword() {
-		return password;
-	}
+        public User(String userName, String password) {
+                this.password = password;
+                this.userName = userName;
+        }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public String getUserName() {
+                return userName;
+        }
+
+        public void setUserName(String userName) {
+                this.userName = userName;
+        }
+
+        public String getPassword() {
+                return password;
+        }
+
+        public void setPassword(String password) {
+                this.password = password;
+        }
 
 }
