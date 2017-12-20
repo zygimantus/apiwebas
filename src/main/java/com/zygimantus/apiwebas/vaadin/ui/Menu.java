3617e41e0f83411dd5b65c778a15ad2896e7cc82
Zygimantus <zygimantus@gmail.com>
2017-12-20 17:39:40 +0200

    Add: Export dump file of DB

--- src/main/java/com/zygimantus/apiwebas/vaadin/ui/Menu.java
+++ src/main/java/com/zygimantus/apiwebas/vaadin/ui/Menu.java
@@ -1,7 +1,15 @@
 package com.zygimantus.apiwebas.vaadin.ui;
 
+import com.vaadin.server.FileResource;
+import com.vaadin.server.Page;
 import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.UI;
+import com.zygimantus.apiwebas.vaadin.util.Db2Sql;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
 
 /**
  *
@@ -17,10 +25,10 @@ public final class Menu extends MenuBar {
                 //
                 MenuItem mainItem = addItem("Apis", null);
                 mainItem.addItem("Swapi", (MenuItem selectedItem) -> {
-                        UI.getCurrent().getNavigator().navigateTo(SwapiView.VIEW_NAME);                        
+                        UI.getCurrent().getNavigator().navigateTo(SwapiView.VIEW_NAME);
                 });
                 mainItem.addItem("MovieDB", (MenuItem selectedItem) -> {
-                        UI.getCurrent().getNavigator().navigateTo(MovieDBView.VIEW_NAME);                        
+                        UI.getCurrent().getNavigator().navigateTo(MovieDBView.VIEW_NAME);
                 });
                 mainItem.addItem("Test", (MenuItem selectedItem) -> {
                 });
@@ -29,7 +37,25 @@ public final class Menu extends MenuBar {
                 //
                 MenuItem toolsItem = addItem("Tools", null);
                 toolsItem.addItem("Export to CSV", (MenuItem selectedItem) -> {
-                        UI.getCurrent().getNavigator().navigateTo(ExportView.VIEW_NAME);                        
+                        UI.getCurrent().getNavigator().navigateTo(ExportView.VIEW_NAME);
+                });
+                toolsItem.addItem("Create MySQL Dump", (MenuItem selectedItem) -> {
+                        ClassLoader classLoader = Menu.class.getClassLoader();
+                        File file = new File(classLoader.getResource("db2sql.properties").getFile());
+                        String sFile = file.getAbsolutePath();
+
+                        Properties props = new Properties();
+                        try {
+                                props.load(new FileInputStream(sFile));
+                                try (PrintWriter out = new PrintWriter("dump.sql")) {
+                                        out.println(Db2Sql.dumpDB(props));
+                                }
+
+                                FileResource res = new FileResource(new File(System.getProperty("user.dir") + "/dump.sql"));
+                                Page.getCurrent().open(res, null, false);
+                        } catch (IOException e) {
+                                System.err.println("Unable to open property file: " + sFile + " exception: " + e);
+                        }
                 });
         }
 }
