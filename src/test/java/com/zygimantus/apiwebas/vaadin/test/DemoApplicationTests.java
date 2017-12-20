a6bd69804fc078d4376835e53cd83fdba4452322
Zygimantus <zygimantus@gmail.com>
2017-12-20 14:20:01 +0200

    Add: DemoApplicationTests

---
+++ src/test/java/com/zygimantus/apiwebas/vaadin/test/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zygimantus.apiwebas.vaadin.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+        @Test
+        public void contextLoads() {
+        }
+
+}
