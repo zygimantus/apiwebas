9570c37e6f91b84fb963533646170dbb602015d6
Zygimantus <zygimantus@gmail.com>
2018-05-11 15:33:06 +0300

    Pagination with SportDeerFixtures data

--- src/main/java/com/zygimantus/apiwebas/vaadin/ui/SportDeerView.java
+++ src/main/java/com/zygimantus/apiwebas/vaadin/ui/SportDeerView.java
@@ -1,12 +1,17 @@
 package com.zygimantus.apiwebas.vaadin.ui;
 
+import com.vaadin.addon.pagination.Pagination;
+import com.vaadin.addon.pagination.PaginationResource;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.Grid;
 import com.zygimantus.sportdeerclient.Doc;
+import com.zygimantus.sportdeerclient.Doc_;
 import com.zygimantus.sportdeerclient.SportDeer;
 import com.zygimantus.sportdeerclient.SportDeerApi;
 import com.zygimantus.sportdeerclient.SportDeerCountries;
+import com.zygimantus.sportdeerclient.SportDeerFixtures;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.logging.Level;
@@ -21,46 +26,80 @@ import retrofit2.Response;
 @SpringView(name = SportDeerView.VIEW_NAME)
 public final class SportDeerView extends ApiView {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public static final String VIEW_NAME = "sportDeer";
+	public static final String VIEW_NAME = "sportDeer";
 
-    private Grid<Doc> grid;
+	private Grid<Doc_> grid;
 
-    @Value("${sportDeerRefreshToken}")
-    private String sportDeerRefreshToken;
+	@Value("${sportDeerRefreshToken}")
+	private String sportDeerRefreshToken;
 
-    @Override
-    public void init() {
+	private int currentPage = 1;
 
-        super.init();
+	@Override
+	public void init() {
 
-        grid = new Grid<>(Doc.class);
+		super.init();
 
-        addComponent(grid);
+		grid = new Grid<>(Doc_.class);
 
-        grid.setWidth("100%");
+		addComponent(grid);
 
-    }
+		grid.setWidth("100%");
 
-    @Override
-    public void enter(ViewChangeEvent event) {
+	}
 
-        try {
-            SportDeerApi.init();
-            SportDeer api = SportDeerApi.getApi();
+	@Override
+	public void enter(ViewChangeEvent event) {
 
-            Response<com.zygimantus.sportdeerclient.SportDeerAccessToken> response = api.getAccessToken(sportDeerRefreshToken).execute();
+		final Pagination pagination = createPagination(1000, currentPage, 10);
+		pagination.addPageChangeListener(e -> {
 
-            Response<SportDeerCountries> response1 = api.getCountries(response.body().getNewAccessToken()).execute();
+			pagination.setTotalCount(listData(e.page()));
 
-            List list = response1.body().getDocs();
-            grid.setItems(list);
+			currentPage = e.page();
+		});
 
-//        save(list, Resource.FOOTBALL_COUNTRIES);
-        } catch (IOException ex) {
-            Logger.getLogger(SportDeerView.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+		addComponent(pagination);
+
+		listData(currentPage);
+
+		// save(list, Resource.FOOTBALL_COUNTRIES);
+	}
+
+	private int listData(int page) {
+
+		SportDeerApi.init();
+		SportDeer api = SportDeerApi.getApi();
+
+		Response<com.zygimantus.sportdeerclient.SportDeerAccessToken> response;
+		try {
+			response = api.getAccessToken(sportDeerRefreshToken).execute();
+
+			Response<SportDeerFixtures> response1;
+			response1 = api.getFixtures(null, null, response.body().getNewAccessToken(), page).execute();
+			List list = response1.body().getDocs();
+
+			grid.setItems(list);
+
+			return response1.body().getPagination().getTotal();
+
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		return 0;
+	}
+
+	private Pagination createPagination(long total, int page, int limit) {
+		final PaginationResource paginationResource = PaginationResource.newBuilder().setTotal(total).setPage(page)
+				.setLimit(limit).build();
+		final Pagination pagination = new Pagination(paginationResource);
+		// TODO make constant in SportDeerClient
+		pagination.setItemsPerPage(30);
+		return pagination;
+	}
 
 }
