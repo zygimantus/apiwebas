d881a14a985c02a3c15fb543bfac92251d095a5b
Zygimantus <zygimantus@gmail.com>
2018-05-30 17:19:06 +0300

    Heroku profile setup

--- src/main/java/com/zygimantus/apiwebas/vaadin/SampleVaadinApplication.java
+++ src/main/java/com/zygimantus/apiwebas/vaadin/SampleVaadinApplication.java
@@ -1,18 +1,36 @@
 package com.zygimantus.apiwebas.vaadin;
 
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @ComponentScan(basePackages = "com.zygimantus.apiwebas.vaadin")
 public class SampleVaadinApplication {
 
-        private static final Logger LOGGER = LoggerFactory.getLogger(SampleVaadinApplication.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(SampleVaadinApplication.class);
 
-        public static void main(String[] args) {
-                SpringApplication.run(SampleVaadinApplication.class, args);
-        }
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(SampleVaadinApplication.class, args);
+		LOGGER.info("Application started, active profiles are:");
+
+		for (String profile : context.getEnvironment().getActiveProfiles()) {
+			LOGGER.info(">>> " + profile);
+		}
+
+	}
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server h2Server() throws SQLException {
+		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+	}
 }
